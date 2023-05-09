c29e354603f11b7274b8643c4934e2101e716d28
Volpi Stefano <101709267+VolpiSte@users.noreply.github.com>
2023-05-09 06:42:18 +0000

    update 0.2

--- src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
+++ src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
@@ -1,11 +1,18 @@
 package it.paleocapa.mastroiannim;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
@@ -44,6 +51,8 @@ public class JavaBossBot extends TelegramLongPollingBot {
 
 	private Map<String, Double> menu = new HashMap<>();
 
+	private double soldi;
+
 	public JavaBossBot() {
 		super(botToken);
 		// Aggiungi i prodotti e i prezzi al menu
@@ -54,68 +63,47 @@ public class JavaBossBot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		    if (update.hasMessage() && update.getMessage().hasText()) {
-        	String messageText = update.getMessage().getText();
-        	long chatId = update.getMessage().getChatId();
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			String messageText = update.getMessage().getText();
+			long chatId = update.getMessage().getChatId();
 
-			if (command.equals("/run")) {
+			if (messageText.equals("/run")) {
 				String message = "Ciao";
 				SendMessage response = new SendMessage();
-				response.setChatId(update.getMessage().getChatId().toString());
+				response.setChatId(chatId);
 				response.setText(message);
-				try{
-					execute(response);
-				} catch (TelegramApiException E){
-					E.printStackTrace();
-				}
-			} else if (command.equals("/ordina")) {
-				SendMessage response = new SendMessage();
-				response.setChatId(update.getMessage().getChatId().toString());
-				response.setText("Scegli il tuo ordine:");
-
-				ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-				keyboardMarkup.setSelective(true);
-				keyboardMarkup.setResizeKeyboard(true);
-				keyboardMarkup.setOneTimeKeyboard(true);
-
-				List<KeyboardRow> keyboard = new ArrayList<>();
-				for (String item : menu.keySet()) {
-					KeyboardRow row = new KeyboardRow();
-					row.add(item);
-					keyboard.add(row);
-				}
-
-				keyboardMarkup.setKeyboard(keyboard);
-				response.setReplyMarkup(keyboardMarkup);
-
-				try{
-					execute(response);
-				} catch (TelegramApiException E){
-					E.printStackTrace();
-				}
-			}else if (messageText.startsWith("/soldi")) {
 				try {
-					double amount = Double.parseDouble(messageText.split(" ")[1].replace(",", "."));
-					soldi += amount;
-					SendMessage response = new SendMessage(chatId, "Soldi aggiunti correttamente!");
 					execute(response);
-				} catch (Exception e) {
-					SendMessage response = new SendMessage(chatId, "Formato denaro non valido!");
-					execute(response);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
 				}
-        	} else if (menu.containsKey(command)) {
-				double price = menu.get(command);
-
-				SendMessage response = new SendMessage();
-				response.setChatId(update.getMessage().getChatId().toString());
-				response.setText(String.format("Hai ordinato %s per %.2f euro.", command, price));
-
-				try{
-					execute(response);
-				} catch (TelegramApiException E){
-					E.printStackTrace();
+			} else if (messageText.startsWith("/soldi ")) {
+				try {
+					String soldiString = messageText.substring(7); // estrae la quantità di denaro dal messaggio
+					double soldi = Double.parseDouble(soldiString.replace(",", ".")); // converte la quantità di denaro in un numero decimale
+					this.soldi = soldi; // assegna la quantità di denaro alla variabile soldi
+					String message = "Hai inserito " + soldi + " €";
+					SendMessage response = new SendMessage();
+					response.setChatId(chatId);
+					response.setText(message);
+					try {
+						execute(response);
+					} catch (TelegramApiException e) {
+						e.printStackTrace();
+					}
+				} catch (NumberFormatException e) {
+					String message = "Formato denaro non valido. Inserisci la quantità di denaro nel formato xx,xx €";
+					SendMessage response = new SendMessage();
+					response.setChatId(chatId);
+					response.setText(message);
+					try {
+						execute(response);
+					} catch (TelegramApiException ex) {
+						ex.printStackTrace();
+					}
 				}
 			}
 		}
 	}
+
 }
